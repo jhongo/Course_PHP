0e8d3604f67436cf5dab8a57541d90bb2553459b
jhongo <jhon_gom22@outlook.com>
2022-10-26 00:34:44 -0500

    Completed E13 - Ciclo For/ tabla de multiplicar

--- ejercicio13.php
+++ ejercicio13.php
@@ -2,10 +2,13 @@
 
     if ($_POST) {
         
-        $limite = $_POST['numLimite'];
+        $multiplicador = $_POST['multiplicador'];
+        $multiplicado = $_POST['multiplicado'];
 
-        for ($i=0; $i < $limite; $i++) { 
-            echo $i."<br/>";
+        for ($i=0; $i <= $multiplicado; $i++) { 
+
+            $resp = $i * $multiplicador;
+            echo $i. "*".$multiplicador."=".$resp."<br/>";
     
         } 
 
@@ -22,14 +25,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Ciclo FOR</title>
 </head>
 <body>
 
-    <form method="post" action="ejecicio13.php">
-    <input type="number" name="numLimite" id="">
-    <input type="submit" value="Enviar">
+<div class="w-full max-w-xs">
+    <form action="ejercicio13.php" method="post"  class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+          Tabla del
+        </label>
+        <input name="multiplicador" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text">
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+          Imprimir hasta
+        </label>
+        <input name="multiplicado" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text">
+      </div>
+      <div class="flex items-center justify-between">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" value="Enviar" >
+          Calcular
+        </button>
+      </div>
     </form>
+  </div>
     
 </body>
 </html>
