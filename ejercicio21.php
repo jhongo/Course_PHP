ee1e62c152cdfebd6ed83ec0549abfae7d3c658c
jhongo <jhon_gom22@outlook.com>
2022-11-02 00:57:04 -0500

    Mapear arrays con los ciclos for y foreach

--- ejercicio21.php
+++ ejercicio21.php
@@ -1,9 +1,27 @@
 <?php
 
-    $fruits=array("f"=>"Fresa","m"=>"Manzana","p"=>"Pera");
-
-    print_r($fruits);
-
     // Esto permite tener en los arreglos indices no numericos
+        $fruits=array("Fresa","Manzana","Pera");
+        // print_r($fruits);
+
+    // echo $fruits[2];
+
+    // Mapear Array por FOR
+    for ($indice=0; $indice < 3; $indice++) {
+        
+        print_r($fruits[$indice]."<br/>");
+    }
+
+    // Mapear Array por FOREACH
+    foreach ($fruits as $indice => &$fruit) {
+        
+        // echo $indice." => ".$fruit."<br/>";
+        echo $fruits[$indice]."<br/>";
+        
+    
+    }
+
+
+
 
 ?>
